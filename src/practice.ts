5071233d69a3b1cba2ec26ff4190e78dd09fb13e
Leejunyoung <39412219+moko0428@users.noreply.github.com>
2023-04-18 13:35:34 +0900

    일반 객체를 interface로 타입 설정하기

--- src/practice.ts
+++ src/practice.ts
@@ -1,34 +1,19 @@
-interface Shape {
-  getArea(): number;
+interface Person {
+  name: string;
+  age?: number; //?의 의미는 설정을 해도 되고 안해도 되는 값으로 선택 옵션.
+}
+interface Developer extends Person {
+  //interface를 extends해서 상속받기
+  skills: string[];
 }
 
-class Circle implements Shape {
-  constructor(public radius: number) {
-    this.radius = radius;
-  }
+const person: Person = {
+  name: "이준영",
+  age: 25,
+};
 
-  getArea() {
-    return this.radius * this.radius * Math.PI;
-  }
-}
-
-class Rectangle implements Shape {
-  constructor(private width: number, private height: number) {
-    this.width = width;
-    this.height = height;
-  }
-  getArea() {
-    return this.width * this.height;
-  }
-}
-const circle = new Circle(5);
-const rectangle = new Rectangle(10, 5);
-
-console.log(circle.radius);
-console.log(rectangle.width);
-
-const shapes: Shape[] = [new Circle(5), new Rectangle(10, 5)];
-
-shapes.forEach((shape) => {
-  console.log(shape.getArea());
-});
+const expert: Developer = {
+  name: "이개발",
+  skills: ["javascript", "react"],
+};
+const people: Person[] = [person, expert];
